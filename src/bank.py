caf4fd693724a2e34efcf11837a1dc81df845c92
Michali K <michali@michalikons.com>
2020-05-03 09:59:27 +1000

    bank transfer

--- src/bank.py
+++ src/bank.py
@@ -5,3 +5,12 @@ class Bank():
 
     def withdraw_from_account(self, account, amount):
         account._withdraw(amount)
+
+    def e_transfer(self, account_from, account_to, amount):
+        initial_from_balance = account_from.balance
+        account_from._withdraw(amount)
+
+        if (account_from.balance == initial_from_balance):
+            return
+
+        account_to._deposit(amount)
