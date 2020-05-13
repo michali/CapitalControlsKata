fd25000cf65b5e1498f089812f190e47cf16d6b3
Michali K <michali@michalikons.com>
2020-05-13 20:45:36 +1000

    Fixed tests

--- tests/test_bank.py
+++ tests/test_bank.py
@@ -147,34 +147,16 @@ class BankTest(unittest.TestCase):
        (60,1),
        (40,30)
     ])
-    def test_withdrawal_over_daily_amount_in_multiple_transactions_not_allowed(self, firstwithdrawalamount, secondwithdrawalamount):
+    def test_withdrawal_over_daily_amount_in_multiple_transactions_not_allowed_two_transactions(self, firstwithdrawalamount, secondwithdrawalamount):
         account = Account()
         bank = Bank()
         bank.deposit_to_account(account, 100)
         
-        bank.withdraw_from_account(account, 60)
-        result = bank.withdraw_from_account(account, 1)
+        bank.withdraw_from_account(account, firstwithdrawalamount)
+        result = bank.withdraw_from_account(account, secondwithdrawalamount)
 
         self.assertEqual(OperationResult.NotAllowed, result)
-        self.assertEqual(40, account.Balance)
-
-    
-
-    # def test_withdrawal_over_daily_amount_in_multiple_transactions_across_two_days_is_allowed(self):
-    #     datetimemock = Mock()
-    #     datetimemock.now.return_value = datetime.datetime(2020, 1, 1, 15, 45, 0)
-
-    #     account = Account()
-    #     bank = Bank(datetimemock)
-    #     bank.deposit_to_account(account, 100)
-
-    #     datetimemock.now.return_value = datetime.datetime(2020, 1, 1, 15, 45, 0)
-    #     bank.withdraw_from_account(account, 60)
-
-    #     result = bank.withdraw_from_account(account, 10)
-
-    #     self.assertEqual(OperationResult.Success, result)
-    #     self.assertEqual(30, account.Balance)
+        self.assertEqual(100 - firstwithdrawalamount, account.Balance)
 
     def test_transfer_abroad_not_allowed(self):
         account = Account()
