ce00611919cc48087dc5484a288305578dc436da
Michali K <michali@michalikons.com>
2020-05-15 21:29:47 +1000

    Revert "Refactored to Chain of Responsibility Pattern"
    This reverts commit a85f2c6b6e001a5010e8636d77d8c0d4106dad38.

--- src/bank.py
+++ src/bank.py
@@ -1,20 +1,29 @@
 import datetime
 from .transaction import OperationResult, TransactionType
-from .restriction import *
 
 class Bank():
 
+    __max_daily_limit = 60
+
     def __init__(self, datetimeprovider = datetime.datetime):
         self.__datetimeprovider = datetimeprovider
-        self.__restrictions = DailyLimit().set_next(TransactionsToday(self.__datetimeprovider))
 
     def deposit_to_account(self, account, amount):
         return account._deposit(amount, self.__datetimeprovider.now())
 
     def withdraw_from_account(self, account, amount):
-        if self.__restrictions.is_restricted(account, amount):
+        if (amount > Bank.__max_daily_limit):
             return OperationResult.NotAllowed
-         
+
+        amount_already_drawn = 0
+
+        for trn in account.Transactions:
+            if trn.Type == TransactionType.Debit and trn.DateTime.date() == self.__datetimeprovider.now().date():
+                amount_already_drawn += trn.Amount
+
+        if amount_already_drawn + amount > Bank.__max_daily_limit:
+            return OperationResult.NotAllowed
+            
         return account._withdraw(amount, self.__datetimeprovider.now())
 
     def transfer(self, account_from, account_to, amount):
