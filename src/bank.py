4483a7d6b2432ce0cc1df4c3323cba42496af79a
Michali K <michali@michalikons.com>
2020-05-19 21:49:45 +1000

    Extracted method, permutated variables

--- src/bank.py
+++ src/bank.py
@@ -19,11 +19,14 @@ class Bank():
         return OperationResult.NotAllowed          
 
     def __can_withdraw(self, account, amount):
-        day_of_week_index = self.__datetimeprovider.now().weekday()
         total_deposits_after_cutoff_date = account._get_total_amount_for_credits_on_and_after_date(Bank.__start_date_for_fresh_transactions)
         money_withdrawn_this_week_so_far_for_date = account._get_withdrawn_amount_this_week_so_far_for_date(self.__datetimeprovider.now())
-        return amount <= total_deposits_after_cutoff_date + Bank.__max_daily_limit * (day_of_week_index + 1) - money_withdrawn_this_week_so_far_for_date
+        return amount <= total_deposits_after_cutoff_date + self.__calculate_max_withdrawal_limit_on_current_day_of_week() - money_withdrawn_this_week_so_far_for_date
     
+    def __calculate_max_withdrawal_limit_on_current_day_of_week(self):
+        day_of_week_index = self.__datetimeprovider.now().weekday()
+        return Bank.__max_daily_limit * (day_of_week_index + 1)
+
     def transfer(self, account_from, account_to, amount):
         date = self.__datetimeprovider.now()
         withdrawal = account_from._transfer_domestic(amount, date)
@@ -42,4 +45,4 @@ class Bank():
     def __can_transfer_abroad(self, account, amount):
         total_deposits_after_cutoff_date = account._get_total_amount_for_credits_on_and_after_date(Bank.__start_date_for_fresh_transactions)
         money_transfered_abroad_this_week = account._get_amount_transfered_abroad_this_week_so_far_for_date(self.__datetimeprovider.now())
-        return amount <= Bank.__max_weekly_bank_transfer_abroad_limit + total_deposits_after_cutoff_date - money_transfered_abroad_this_week
+        return amount <= total_deposits_after_cutoff_date + Bank.__max_weekly_bank_transfer_abroad_limit - money_transfered_abroad_this_week
